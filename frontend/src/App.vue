9281da0a036c2f2295d4fc156e16a2ec86b98cf0
paolina92 <paolinardc@gmail.com>
2025-04-24 21:29:16 +0200

    wip

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,27 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Button from './components/Button/index.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Button
+  },
+  setup() {
+    const message = ref('Hello world Pauline')
+    
+    return {
+      message
+    }
+  }
+})
+</script>
+
 <template>
   <div class="flex flex-col items-center justify-center h-screen">
-    <div class="text-4xl">Hello world Pauline</div>
-    <div class="text-4xl">Hello world Pauline</div>
+    <div class="text-4xl">{{ message }}</div>
+    <div class="text-4xl">{{ message }}</div>
     <div>Hello</div>
+    <Button />
   </div>
 </template>
---
+++ frontend/src/components/Button/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <button class="bg-blue-500 text-white p-2 rounded-md">Click me</button>
+</template>
