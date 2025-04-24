615896d1f65e02437b6e65fbb7f4255a2f2a5293
paolina92 <paolinardc@gmail.com>
2025-04-24 20:07:07 +0200

    lint and format

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   server: {
     open: true,
   },
-});
+})
