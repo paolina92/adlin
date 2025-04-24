5b5a2caa7eca4dd177302ea9bdee809ef010edf7
paolina92 <paolinardc@gmail.com>
2025-04-24 20:00:22 +0200

    install prettier and eslint

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { VueQueryPlugin } from "@tanstack/vue-query";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import './style.css'
+import App from './App.vue'
 
-const pinia = createPinia();
-const app = createApp(App);
-app.use(pinia);
-app.use(VueQueryPlugin);
-app.mount("#app");
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+app.use(VueQueryPlugin)
+app.mount('#app')
